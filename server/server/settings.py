3d345b35d5db488f381ae443d0cbb5abf6620664
aayushker <singhaayushker@gmail.com>
2024-06-29 13:46:21 +0530

    Added the credentials to .env and updated the settings.py file | Initated gitignore

--- server/server/settings.py
+++ server/server/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,8 +28,14 @@ SECRET_KEY = 'django-insecure-=f#vc(!=udp-9-7$*h%-)w=d5v_)#%z61db^8_ofb=u(qn-5wb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
+ALLOWED_HOSTS = []
 
 # Application definition
 
